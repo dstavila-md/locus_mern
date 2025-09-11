18160791b0ec18abd4af98691a6fcef3b485d546
XenderMd <denis.stavilamd@gmail.com>
2025-09-11 17:57:03 +0300

    Adding the usersList page and component

--- FrontEnd/src/users/pages/Users.js
+++ FrontEnd/src/users/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <div>Users ..</div>;
+  const USERS = [
+    {
+      id: 'u1',
+      image:
+        'https://uploads.builtforbackroads.com/uploads/2022/02/2022.02.15-HONDA-CIVIC-2017_2-1536x1024.jpg',
+      place: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
