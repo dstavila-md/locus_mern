15f650d19aff7870797d046c92fdafcbc770bfdc
XenderMd <denis.stavilamd@gmail.com>
2025-09-22 16:56:22 +0300

    Adding place specific routes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,6 +5,6 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = epxress();
 
-app.use(placesRoutes);
+app.use('/api/places', placesRoutes);
 
 app.listen(5000);
--- BackEnd/routes/places-routes.js
+++ BackEnd/routes/places-routes.js
@@ -2,9 +2,33 @@ const express = require('express');
 
 const router = express.Router();
 
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous places in the world',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1',
+  },
+];
+
 router.get('/', (req, res, next) => {
   console.log('GET Request in Places');
   res.json({ message: 'It works!' });
+});
+
+router.get('/:placeId', (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  if (place) {
+    res.send({ place });
+  } else {
+    res.status(404).send({ place: 'Not found' });
+  }
 });
 
 module.exports = router;
