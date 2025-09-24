5f8dfb69ba6f96fc7536600358f8c7e84cd01ef6
XenderMd <denis.stavilamd@gmail.com>
2025-09-24 15:48:18 +0300

    Validating API input (request bodies)

--- BackEnd/routes/places-routes.js
+++ BackEnd/routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const {
   getPlaceById,
@@ -16,7 +17,14 @@ router.get('/:placeId', getPlaceById);
 
 router.get('/user/:userId', getPlacesByUserId);
 
-router.post('/', createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 }, check('address').not().isEmpty()),
+  ],
+  createPlace
+);
 
 router.patch('/:placeId', updatePlaceById);
 
