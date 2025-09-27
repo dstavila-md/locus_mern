7981e88906284af884960c01665cf439c92974e9
XenderMd <denis.stavilamd@gmail.com>
2025-09-27 22:46:44 +0300

    using the user model for signup

--- BackEnd/controllers/users-controllers.js
+++ BackEnd/controllers/users-controllers.js
@@ -1,7 +1,9 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
+const User = require('../models/user');
 
 const HttpError = require('../models/http-error');
+const user = require('../models/user');
 
 const DUMMY_USERS = [
   {
@@ -16,25 +18,48 @@ const getUsers = (req, res, next) => {
   res.status(200).json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, image } = req.body;
 
-  const userExists = DUMMY_USERS.find((user) => user.email === email);
-  if (!userExists) {
-    const createdUser = { id: uuidv4(), name, email, password };
-
-    DUMMY_USERS.push(createdUser);
-
-    res.status(201).json({ user: createdUser });
-  } else {
-    throw new HttpError('Could not create user, email already exists', 422);
+  let userExists;
+  try {
+    userExists = await User.findOne({ email: email });
+  } catch (error) {
+    return next(
+      new HttpError('Creating user failed, please try again later.', 500)
+    );
   }
+
+  if (userExists) {
+    return next(
+      new HttpError('User exists already, please login instead.', 422)
+    );
+  }
+
+  let createdUser;
+  try {
+    createdUser = new User({
+      name,
+      email,
+      password,
+      image: image || 'https://picsum.photos/200',
+      places: '[]',
+    });
+    await createdUser.save();
+  } catch (error) {
+    return next(
+      new HttpError('Could not create user, please try again later.', 500)
+    );
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
